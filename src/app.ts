e4703680c20cb3057b6596f77a99a90535eb29c9
Mourão <p3dr0sk0sk7160@gmail.com>
2026-05-17 14:21:45 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,66 @@
+// @ts-ignore: missing type declarations for express
+import express from "express";
+
+// Create an instance of the Express application and configure it to parse JSON request bodies
+const app: express.Application = express();
+app.use(express.json());
+
+// Sample data: an array of book objects
+const books: any[] = [
+  {
+    id: 1,
+    title: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    title: "Harry Potter e a Pedra Filosofal",
+  },
+];
+
+// Define a route handler for the root URL ("/")
+app.get("/", (req: express.Request, res: express.Response): void => {
+  res.status(200).send("Bem-vindo à API de Livros!");
+});
+
+// Define a route handler for the "/livros" URL that returns the list of books as JSON
+app.get("/livros", (req: express.Request, res: express.Response): void => {
+  res.status(200).json(books);
+});
+
+// Define a route handler for the "/livros/:id" URL that returns a specific book based on its ID
+app.get("/livros/:id", (req: express.Request, res: express.Response): void => {
+  const index: Number = searchBooks(req.params.id);
+  res.status(200).json(books[index as number]);
+});
+
+// Define a route handler for the "/livros" URL that allows adding a new book to the list
+app.post("/livros", (req: express.Request, res: express.Response): void => {
+  books.push(req.body);
+  res.status(201).send("Livro adicionado com sucesso!");
+});
+
+// Define a route handler for the "/livros/:id" URL that allows updating an existing book based on its ID
+app.put("/livros/:id", (req: express.Request, res: express.Response): void => {
+  const index: Number = searchBooks(req.params.id);
+  books[index as number].title = req.body.title;
+  res.status(200).send("Livro atualizado com sucesso!");
+});
+
+// Define a route handler for the "/livros/:id" URL that allows deleting a book based on its ID
+app.delete(
+  "/livros/:id",
+  (req: express.Request, res: express.Response): void => {
+    const index: Number = searchBooks(req.params.id);
+    books.splice(index as number, 1);
+    res.status(200).send("Livro deletado com sucesso!");
+  },
+);
+
+// Helper function to search for a book by its ID and return its index in the array
+function searchBooks(id: string): Number {
+  return books.findIndex((b) => {
+    return b.id === Number(id);
+  });
+}
+
+export default app;
